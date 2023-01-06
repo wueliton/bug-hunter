6c61781857c7755263dab2a992c49dc8dd16d43c
Paulo Horacio <paulo.wueliton@hotmail.com>
2023-01-06 18:12:22 -0300

    fix: vite build fix static files copy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   base: '/bug-hunter',
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/**',
+          dest: 'assets',
+        },
+      ],
+    }),
+  ],
 });
