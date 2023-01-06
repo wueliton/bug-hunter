36e6287a6af5b401bcac7bef83b746f38f2ba648
Paulo Horacio <paulo.wueliton@hotmail.com>
2023-01-06 11:41:38 -0300

    🐞 fix: github pages deploy

--- src/assets/tileset/paulo_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="paulo_tileset" tilewidth="12" tileheight="12" tilecount="182" columns="14">
- <image source="../images/tileset.png" width="168" height="160"/>
-</tileset>
